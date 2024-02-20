a2947cf8c45cbd1f66e6c83d31fe4c11de81a321
GennadySht <gennady.sht@gmail.com>
2024-02-20 14:02:31 +0200

    methods changed to streems writeble

--- HW/project_hw5/controllers/uploadController.js
+++ HW/project_hw5/controllers/uploadController.js
@@ -1,6 +1,7 @@
-const multer = require("multer");
-const config = require("../config/config");
-const utils = require("../utils/utils");
+const { Readable, Writable, Duplex, Transform } = require('stream');
+const multer = require('multer');
+const config = require('../config/config');
+const utils = require('../utils/utils');
 
 const uploadDir = utils.createUploadsDir();
 
@@ -8,9 +9,11 @@ const storage = multer.diskStorage({
   destination: (req, file, cb) => {
     cb(null, uploadDir);
   },
+
   filename: (req, file, cb) => {
-    cb(null, /*utils.generateFileName(*/ file.originalname);
+    cb(null, file.originalname);
   },
+
 });
 
 const upload = multer({
@@ -18,27 +21,45 @@ const upload = multer({
   limits: {
     fileSize: config.maxFileSize,
   },
-}).single("video");
+}).single('video');
+
 
 const handleUpload = (req, res) => {
   upload(req, res, (err) => {
     if (err) {
       return res
         .status(500)
-        .json({ success: false, message: "Помилка завантаження відео." });
+        .json({ success: false, message: 'Помилка завантаження відео.' });
     }
 
     if (req.file) {
-      return res.json({
-        success: true,
-        message: "200  Відео успішно завантажено!",
+      const readableStream = new Readable({
+        read() {
+          this.push(req.file.buffer);
+          this.push(null);
+        },
+      });
+
+      const writableStream = new Writable({
+        write(chunk, encoding, callback) {
+          callback();
+        },
+      });
+
+      readableStream.pipe(writableStream);
+
+      writableStream.on('finish', () => {
+        return res.json({
+          success: true,
+          message: '200  Відео успішно завантажено!',
+        });
       });
     } else {
-      return res.json({ success: false, message: "Файли не завантажені" });
+      return res.json({ success: false, message: 'Файли не завантажені' });
     }
   });
 };
 
-module.exports = {
-  handleUpload,
-};
+module.exports = { handleUpload };
+
+
