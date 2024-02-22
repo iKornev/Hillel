34958310bfc9f28cf52d8f09056bc227b45efde0
Ivan Kornev <ivankornev@MacBook-Pro-Ivan.local>
2024-02-22 09:40:10 +0200

    Added examples hor WH 7

--- lesson-7/Routes/userRouter.js
+++ lesson-7/Routes/userRouter.js
@@ -1,13 +1,58 @@
 import { Router } from 'express';
+import { validation } from "../middleware/validationMiddleware.js";
+import { validator } from "../middleware/unifyValidator.js";
+import { userCreateValidationSchema, userUpdateValidationSchema } from "../validationSchemas/userValidationSchemas.js";
+import bcrypt from 'bcrypt'
 
 const userRouter = new Router()
+let user = null
+
+const paswordHashFromBD =  '$argon2id$v=19$m=65536,t=3,p=4$Te3VDDb2+jAB/OL0uC0ncg$4ur+Qa9JVZpgVMFg0UY2J0GB3wXWfPTWlCTr8GkCZR0'
 
 userRouter.get('/user', (req, res) => {
     res.send('some rosdacascascasuter resp')
 })
 
-userRouter.post('/user', (req, res) => {
+userRouter.put('/user', validator(userUpdateValidationSchema), (req, res) => {
+    res.send('API for user update')
+})
+
+userRouter.post('/user', validator(userCreateValidationSchema), async (req, res) => {
+
+    const {password, username} = req.body
+
+
+    const saltRounds = 10;
+    const result = await bcrypt.hash(password, saltRounds)
+
+    console.log({result})
     res.send('API for user creation')
 })
+
+userRouter.post('auth/user', validator(userCreateValidationSchema), async (req, res) => {
+
+
+    const {password, username} = req.body
+
+    try {
+        const result = await argon2.verify(paswordHashFromBD, password)
+        if (result) {
+            console.log('password match')
+        } else {
+            console.log(' password did not match')
+        }
+    } catch (err) {
+        console.log({err})
+    }
+    console.log({hash})
+
+
+    res.send('API for user creation')
+})
+
+
+
+
+
 
 export { userRouter }
--- lesson-7/index.js
+++ lesson-7/index.js
@@ -1,10 +1,12 @@
 import express from 'express'
-import { router } from "./Routes/routes.js";
 import { userRouter } from "./Routes/userRouter.js";
+import {ErrorHandler} from './middleware/ErrorHandler.js'
 
 const app = express()
 app.use(express.json())
-app.use(router, userRouter)
+app.use( userRouter)
+
+app.use(ErrorHandler)
 
 
 app.listen(3000, () => {
